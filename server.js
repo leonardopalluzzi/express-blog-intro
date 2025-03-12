e707ccd835d61f93becd787151ed096024e372e4
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-12 14:42:48 +0100

    add base server base structure

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express'); //express library import
 const app = express; // express app initialization
 const port = 3000; // port assignation
+
+// set static assets
+app.use(express.static('images'));
+
+// start server listen
+app.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`);
+});
+
+// routing
+app.get('/api/v1/blog', (req, res) => {
+    res.send('Server del mio blog');
+})
