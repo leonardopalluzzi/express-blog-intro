e1b536dea3f4729ce32d396e021e906bc22a45d9
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-12 15:08:05 +0100

    add data obj and response send in json format

--- server.js
+++ server.js
@@ -22,34 +22,41 @@ app.get('/bacheca', (req, res) => {
         {
             title: 'Ciambellone',
             content: 'A delicious and fluffy Italian ring cake perfect for breakfast or dessert.',
-            img: './cimabellone.jpg',
+            img: '/ciambellone.jpeg',
             tags: ['dessert', 'cake', 'Italian', 'breakfast']
         },
         {
             title: 'Cracker alla barbabietola',
             content: 'Healthy and crunchy beetroot crackers, a perfect snack for any time of the day.',
-            img: './cracker_barbabietola.jpeg',
+            img: '/cracker_barbabietola.jpeg',
             tags: ['snack', 'healthy', 'beetroot', 'crunchy']
         },
         {
             title: 'Pane fritto dolce',
             content: 'Sweet fried bread, a traditional treat that is crispy on the outside and soft on the inside.',
-            img: './pane_fritto.jpeg',
+            img: '/pane_fritto.jpeg',
             tags: ['sweet', 'fried', 'bread', 'traditional']
         },
         {
             title: 'Pasta alla barbabietola',
             content: 'A vibrant and flavorful beetroot pasta dish that is both healthy and delicious.',
-            img: './pasta_barbabietola.jpeg',
+            img: '/pasta_barbabietola.jpeg',
             tags: ['pasta', 'beetroot', 'healthy', 'flavorful']
         },
         {
             title: 'Torta paesana',
             content: 'A rich and moist country-style cake made with simple ingredients and lots of love.',
-            img: './torta_paesana.jpeg',
+            img: '/torta_paesana.jpeg',
             tags: ['cake', 'country-style', 'rich', 'moist']
         }
     ]
 
+    const data = [
+        {
+            success: true,
+            response: posts
+        }
+    ]
 
+    res.json(data);
 });
